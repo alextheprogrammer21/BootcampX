2bec256873baf087ae149582962764abfc1480f6
Alex <alextheprogrammer21@gmail.com>
2020-03-05 18:57:57 +0000

    fixed sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,4 @@
 const { Pool } = require('pg');
-let cohortName = process.argv.slice(2)[0].toLowerCase();
-let resultsNumber = process.argv.slice(2)[1].toLowerCase();
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,17 +6,22 @@ const pool = new Pool({
   database: 'bootcampx'
   
 });
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+
+const innput = process.argv.slice(2);
+const values = [`%${innput}%`];
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`;
+
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+  res.rows.forEach(user => {
+    console.log("success");
   })
-});
+}).catch(err => console.error('query error', err.stack));
